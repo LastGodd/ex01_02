d2d4e64cfb26e683e3dbd3add58bd489e5c4fd01
DESKTOP-UJ7U5SF\etribe <huny@etribe.co.kr>
2022-07-22 10:26:06 +0900

    220722_first

--- ex01_2/src/main/java/com/zerock/controller/SampleController.java
+++ ex01_2/src/main/java/com/zerock/controller/SampleController.java
@@ -2,13 +2,18 @@ package com.zerock.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.zerock.domain.SampleDTO;
 import com.zerock.domain.SampleDTOList;
@@ -93,5 +98,32 @@ public class SampleController {
 		dto.setAge(10);
 		dto.setName("홍길동");
 		return dto;
+	}
+
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07() {
+		log.info("/ex07.....");
+
+		// {"name":"홍길동"}
+		String msg = "{\"name\":\"홍길동\"}";
+
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+
+		return new ResponseEntity<String>(msg, header, HttpStatus.OK);
+	}
+
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("exUpload");
+	}
+
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		files.forEach(file -> {
+			log.info("===========================");
+			log.info("name: " + file.getOriginalFilename());
+			log.info("size: " + file.getSize());
+		});
 	}
 }
